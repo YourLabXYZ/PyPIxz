76d7eaca050358358852f13ca6f2e6e9179aedd2
KDUser <128939316+KDUser12@users.noreply.github.com>
2025-01-19 18:37:21 +0100

    Fix - UPPER_CASE naming style (#66)

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ pypixz.install_modules("pypixz", version_range=">=1.0,!=1.1")
 pypixz.install_requirements("requirements.txt", enable_logging=False)
 
 # Retrieve information from a module.
-result = pypixz.get_module_info("pypixz", version="1.1.2")
-print(result)
+RESULT = pypixz.get_module_info("pypixz", version="1.1.2")
+print(RESULT)
